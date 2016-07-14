1447eef7ca7a4e227348d6b73542272da57cb847
David Pfeiffer <davidjpfeiffer@yahoo.com>
2016-07-14 16:53:50 -0500

    Added IIFE to index.js

--- index.js
+++ index.js
@@ -1,178 +1,181 @@
-var randomScalingFactor = function() {
-	return Math.round(Math.random() * 100)
-};
+(function() {
+    var randomScalingFactor = function() {
+        return Math.round(Math.random() * 100)
+    };
 
-var barGraphData;
+    var barGraphData;
 
-window.httpService.get('barGraphData.json').then(data => barGraphData = data );
+    window.httpService.get('barGraphData.json').then(data => barGraphData = data );
 
-var doughnutGraphData = [
-    {
-        value: 300,
-        color:"#F7464A",
-        highlight: "#FF5A5E",
-        label: "Red"
-    },
-    {
-        value: 50,
-        color: "#46BFBD",
-        highlight: "#5AD3D1",
-        label: "Green"
-    },
-    {
-        value: 100,
-        color: "#FDB45C",
-        highlight: "#FFC870",
-        label: "Yellow"
-    },
-    {
-        value: 40,
-        color: "#949FB1",
-        highlight: "#A8B3C5",
-        label: "Grey"
-    },
-    {
-        value: 120,
-        color: "#4D5360",
-        highlight: "#616774",
-        label: "Dark Grey"
-    }
-]
-
-var lineGraphData = {
-    labels : ["January","February","March","April","May","June","July"],
-    datasets : [
+    var doughnutGraphData = [
         {
-            label: "My First dataset",
-            fillColor : "rgba(220,220,220,0.2)",
-            strokeColor : "rgba(220,220,220,1)",
-            pointColor : "rgba(220,220,220,1)",
-            pointStrokeColor : "#fff",
-            pointHighlightFill : "#fff",
-            pointHighlightStroke : "rgba(220,220,220,1)",
-            data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+            value: 300,
+            color:"#F7464A",
+            highlight: "#FF5A5E",
+            label: "Red"
         },
         {
-            label: "My Second dataset",
-            fillColor : "rgba(151,187,205,0.2)",
-            strokeColor : "rgba(151,187,205,1)",
-            pointColor : "rgba(151,187,205,1)",
-            pointStrokeColor : "#fff",
-            pointHighlightFill : "#fff",
-            pointHighlightStroke : "rgba(151,187,205,1)",
-            data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+            value: 50,
+            color: "#46BFBD",
+            highlight: "#5AD3D1",
+            label: "Green"
+        },
+        {
+            value: 100,
+            color: "#FDB45C",
+            highlight: "#FFC870",
+            label: "Yellow"
+        },
+        {
+            value: 40,
+            color: "#949FB1",
+            highlight: "#A8B3C5",
+            label: "Grey"
+        },
+        {
+            value: 120,
+            color: "#4D5360",
+            highlight: "#616774",
+            label: "Dark Grey"
         }
     ]
-}
 
-var pieGraphData = [
-    {
-        value: 300,
-        color:"#F7464A",
-        highlight: "#FF5A5E",
-        label: "Red"
-    },
-    {
-        value: 50,
-        color: "#46BFBD",
-        highlight: "#5AD3D1",
-        label: "Green"
-    },
-    {
-        value: 100,
-        color: "#FDB45C",
-        highlight: "#FFC870",
-        label: "Yellow"
-    },
-    {
-        value: 40,
-        color: "#949FB1",
-        highlight: "#A8B3C5",
-        label: "Grey"
-    },
-    {
-        value: 120,
-        color: "#4D5360",
-        highlight: "#616774",
-        label: "Dark Grey"
+    var lineGraphData = {
+        labels : ["January","February","March","April","May","June","July"],
+        datasets : [
+            {
+                label: "My First dataset",
+                fillColor : "rgba(220,220,220,0.2)",
+                strokeColor : "rgba(220,220,220,1)",
+                pointColor : "rgba(220,220,220,1)",
+                pointStrokeColor : "#fff",
+                pointHighlightFill : "#fff",
+                pointHighlightStroke : "rgba(220,220,220,1)",
+                data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+            },
+            {
+                label: "My Second dataset",
+                fillColor : "rgba(151,187,205,0.2)",
+                strokeColor : "rgba(151,187,205,1)",
+                pointColor : "rgba(151,187,205,1)",
+                pointStrokeColor : "#fff",
+                pointHighlightFill : "#fff",
+                pointHighlightStroke : "rgba(151,187,205,1)",
+                data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+            }
+        ]
     }
-]
 
-var polarAreaGraphData = [
-    {
-        value: 300,
-        color:"#F7464A",
-        highlight: "#FF5A5E",
-        label: "Red"
-    },
-    {
-        value: 50,
-        color: "#46BFBD",
-        highlight: "#5AD3D1",
-        label: "Green"
-    },
-    {
-        value: 100,
-        color: "#FDB45C",
-        highlight: "#FFC870",
-        label: "Yellow"
-    },
-    {
-        value: 40,
-        color: "#949FB1",
-        highlight: "#A8B3C5",
-        label: "Grey"
-    },
-    {
-        value: 120,
-        color: "#4D5360",
-        highlight: "#616774",
-        label: "Dark Grey"
+    var pieGraphData = [
+        {
+            value: 300,
+            color:"#F7464A",
+            highlight: "#FF5A5E",
+            label: "Red"
+        },
+        {
+            value: 50,
+            color: "#46BFBD",
+            highlight: "#5AD3D1",
+            label: "Green"
+        },
+        {
+            value: 100,
+            color: "#FDB45C",
+            highlight: "#FFC870",
+            label: "Yellow"
+        },
+        {
+            value: 40,
+            color: "#949FB1",
+            highlight: "#A8B3C5",
+            label: "Grey"
+        },
+        {
+            value: 120,
+            color: "#4D5360",
+            highlight: "#616774",
+            label: "Dark Grey"
+        }
+    ]
+
+    var polarAreaGraphData = [
+        {
+            value: 300,
+            color:"#F7464A",
+            highlight: "#FF5A5E",
+            label: "Red"
+        },
+        {
+            value: 50,
+            color: "#46BFBD",
+            highlight: "#5AD3D1",
+            label: "Green"
+        },
+        {
+            value: 100,
+            color: "#FDB45C",
+            highlight: "#FFC870",
+            label: "Yellow"
+        },
+        {
+            value: 40,
+            color: "#949FB1",
+            highlight: "#A8B3C5",
+            label: "Grey"
+        },
+        {
+            value: 120,
+            color: "#4D5360",
+            highlight: "#616774",
+            label: "Dark Grey"
+        }
+    ]
+
+    var radarGraphData = {
+        labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
+        datasets: [
+            {
+                label: "My First dataset",
+                fillColor: "rgba(220,220,220,0.2)",
+                strokeColor: "rgba(220,220,220,1)",
+                pointColor: "rgba(220,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: [65,59,90,81,56,55,40]
+            },
+            {
+                label: "My Second dataset",
+                fillColor: "rgba(151,187,205,0.2)",
+                strokeColor: "rgba(151,187,205,1)",
+                pointColor: "rgba(151,187,205,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(151,187,205,1)",
+                data: [28,48,40,19,96,27,100]
+            }
+        ]
     }
-]
 
-var radarGraphData = {
-	labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
-	datasets: [
-		{
-			label: "My First dataset",
-			fillColor: "rgba(220,220,220,0.2)",
-			strokeColor: "rgba(220,220,220,1)",
-			pointColor: "rgba(220,220,220,1)",
-			pointStrokeColor: "#fff",
-			pointHighlightFill: "#fff",
-			pointHighlightStroke: "rgba(220,220,220,1)",
-			data: [65,59,90,81,56,55,40]
-		},
-		{
-			label: "My Second dataset",
-			fillColor: "rgba(151,187,205,0.2)",
-			strokeColor: "rgba(151,187,205,1)",
-			pointColor: "rgba(151,187,205,1)",
-			pointStrokeColor: "#fff",
-			pointHighlightFill: "#fff",
-			pointHighlightStroke: "rgba(151,187,205,1)",
-			data: [28,48,40,19,96,27,100]
-		}
-	]
-}
+    var barGraph = document.getElementById("bar-graph-canvas").getContext("2d");
+    var doughnutGraph = document.getElementById("doughnut-graph-canvas").getContext("2d");
+    var lineGraph = document.getElementById("line-graph-canvas").getContext("2d");
+    var pieGraph = document.getElementById("pie-graph-canvas").getContext("2d");
+    var polarAreaGraph = document.getElementById("polar-area-graph-canvas").getContext("2d");
+    var radarGraph = document.getElementById("radar-graph-canvas").getContext("2d");
 
-var barGraph = document.getElementById("bar-graph-canvas").getContext("2d");
-var doughnutGraph = document.getElementById("doughnut-graph-canvas").getContext("2d");
-var lineGraph = document.getElementById("line-graph-canvas").getContext("2d");
-var pieGraph = document.getElementById("pie-graph-canvas").getContext("2d");
-var polarAreaGraph = document.getElementById("polar-area-graph-canvas").getContext("2d");
-var radarGraph = document.getElementById("radar-graph-canvas").getContext("2d");
+    var graphOptions = {
+        responsive : true
+    };
 
-var graphOptions = {
-	responsive : true
-};
+    window.onload = function() {
+        window.barGraph = new Chart(barGraph).Bar(barGraphData, graphOptions);
+        window.doughnutGraph = new Chart(doughnutGraph).Doughnut(doughnutGraphData, graphOptions);
+        window.lineGraph = new Chart(lineGraph).Line(lineGraphData, graphOptions);
+        window.pieGraph = new Chart(pieGraph).Pie(pieGraphData, graphOptions);
+        window.polarAreaGraph = new Chart(polarAreaGraph).PolarArea(polarAreaGraphData, graphOptions);
+        window.radarGraph = new Chart(radarGraph).Radar(radarGraphData, graphOptions);
+    }
 
-window.onload = function() {
-	window.barGraph = new Chart(barGraph).Bar(barGraphData, graphOptions);
-	window.doughnutGraph = new Chart(doughnutGraph).Doughnut(doughnutGraphData, graphOptions);
-	window.lineGraph = new Chart(lineGraph).Line(lineGraphData, graphOptions);
-	window.pieGraph = new Chart(pieGraph).Pie(pieGraphData, graphOptions);
-	window.polarAreaGraph = new Chart(polarAreaGraph).PolarArea(polarAreaGraphData, graphOptions);
-	window.radarGraph = new Chart(radarGraph).Radar(radarGraphData, graphOptions);
-}
+}());
