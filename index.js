4df6c4ea2937114c8a103a176b4de93743a71987
David Pfeiffer <davidjpfeiffer@users.noreply.github.com>
2016-07-14 16:25:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,25 +2,8 @@ var randomScalingFactor = function() {
 	return Math.round(Math.random() * 100)
 };
 
-var barGraphData = {
-	labels : ["January","February","March","April","May","June","July"],
-	datasets : [
-		{
-			fillColor : "#ffa500",
-			strokeColor : "rgba(220,220,220,0.8)",
-			highlightFill: "rgba(220,220,220,0.75)",
-			highlightStroke: "rgba(220,220,220,1)",
-			data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
-		},
-		{
-			fillColor : "rgba(151,187,205,0.5)",
-			strokeColor : "rgba(151,187,205,0.8)",
-			highlightFill : "rgba(151,187,205,0.75)",
-			highlightStroke : "rgba(151,187,205,1)",
-			data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
-		}
-	]
-}
+var barGraphData = 
+app.httpService.get('barGraphData.json').then(data => barGraphData = data; );
 
 var doughnutGraphData = [
     {
